63b1a4c3f0ffc750b1ba57904cabd393e8954b51
Alejo Boga <alejoboga20@gmail.com>
2023-09-25 12:22:47 -0300

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       playground: false,
       driver: ApolloDriver,
