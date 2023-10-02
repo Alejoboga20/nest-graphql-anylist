ad379f7a4f8633e8fb760d5557347b6e1ba261f5
Alejo Boga <alejoboga20@gmail.com>
2023-10-02 11:21:20 -0300

    create user and handle error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterInput } from './dto/input/register.input';
 import { AuthResponseType } from './types/auth-response.type';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
   async register(registerInput: RegisterInput): Promise<AuthResponseType> {
-    throw new Error(`Not implemented yet ${JSON.stringify(registerInput)}`);
+    const user = await this.userService.create(registerInput);
+
+    return {
+      user,
+      token: 'token',
+    };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { User } from './entities/user.entity';
+import { RegisterInput } from '../auth/dto/input/register.input';
 
 @Injectable()
 export class UserService {
+  private logger: Logger = new Logger('UserService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(registerInput: RegisterInput): Promise<User> {
+    try {
+      const newUser = this.userRepository.create(registerInput);
+      await this.userRepository.save(newUser);
+
+      return newUser;
+    } catch (error) {
+      this.handleDbErrors(error);
+    }
+  }
+
   async findAll(): Promise<User> {
     throw new Error('Method not implemented.');
   }
@@ -14,5 +40,14 @@ export class UserService {
 
   async block(id: string): Promise<User> {
     throw new Error(`Method not implemented. ${id}`);
+  }
+
+  private handleDbErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('check logs');
   }
 }
