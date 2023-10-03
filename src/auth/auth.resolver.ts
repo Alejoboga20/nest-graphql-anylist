bd336747b9a20216ae0efa9c93b01adb97bb91e2
Alejo Boga <alejoboga20@gmail.com>
2023-10-03 16:23:35 -0300

    custom decorator of custom user

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -5,6 +5,8 @@ import { AuthService } from './auth.service';
 import { RegisterInput, LoginInput } from './dto/input';
 import { AuthResponseType } from './types/auth-response.type';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Resolver()
 export class AuthResolver {
@@ -26,8 +28,8 @@ export class AuthResolver {
 
   @Query(() => AuthResponseType, { name: 'revalidateToken' })
   @UseGuards(JwtAuthGuard)
-  revalidateToken() //@CurrentUser() user: User,
-  : AuthResponseType {
+  revalidateToken(@CurrentUser() user: User): AuthResponseType {
+    console.log({ user });
     throw new Error('Resolver not implemented');
   }
 }
---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,20 @@
+import {
+  ExecutionContext,
+  InternalServerErrorException,
+  createParamDecorator,
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUser = createParamDecorator(
+  (roles = [], context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext().req.user;
+
+    if (!user)
+      throw new InternalServerErrorException(
+        'No user found on request context - Make sure JwtAuthGuard is applied on the resolver',
+      );
+
+    return user;
+  },
+);
